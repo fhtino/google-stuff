4f3f9ff19975ab267c7ea1e9cdfc1cfa65e214a1
fabry <fabry>
2021-01-10 09:36:59 +0100

    first commit

---
+++ AuthBackground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthBackground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is DEMO application");
+        }
+    }
+}
